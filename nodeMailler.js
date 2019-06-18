6c3a845eeeadf7a519e91ba737de0dff050d028e
Marcio Silva <marciotexas@yahoo.com>
2019-06-18 13:56:03 -0500

    commit2

---
+++ nodeMailler.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
+const path = require('path')
+const nodemailer = require('nodemailer');
+
+const app = express();
+
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
